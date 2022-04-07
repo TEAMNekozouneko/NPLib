c68bb14c70034635dbc190535c58f443e3babf6e
Taitaitatata <taitaitatatasub@outlook.jp>
2022-04-07 17:07:39 +0900

    [Update] Util関数作成

--- src/main/java/com/nekozouneko/nplib/NPLib.java
+++ src/main/java/com/nekozouneko/nplib/NPLib.java
@@ -1,8 +1,9 @@
 package com.nekozouneko.nplib;
 
 /**
+ * NPLibはBukkit / Spigotプラグインの簡単なユーティリティ関数軍です。
  * @author Taitaitatata
- * @version 1.0
+ * @version 1.2
  */
 public final class NPLib {
     /**
@@ -16,6 +17,78 @@ public final class NPLib {
             if (str.equalsIgnoreCase(equals_str)) return true;
         }
         return false;
+    }
+
+    /**
+     * ティックから秒数に変換します。
+     * @param tick ティック
+     * @return 秒数
+     */
+    public static double tickToSecond(double tick) {
+        return tick / 20d;
+    }
+
+    /**
+     * ティックから秒数に変換します。
+     * @param tick ティック
+     * @return 秒数
+     */
+    public static double tickToSecond(int tick) {
+        return tick / 20d;
+    }
+
+    /**
+     * ティックから秒数に変換します。
+     * @param tick ティック
+     * @return 秒数
+     */
+    public static double tickToSecond(long tick) {
+        return tick / 20d;
+    }
+
+    /**
+     * ティックから秒数に変換します。
+     * @param tick ティック
+     * @return 秒数
+     */
+    public static double tickToSecond(short tick) {
+        return tick / 20d;
+    }
+
+    /**
+     * 秒数からティックに変換します。
+     * @param second 秒数
+     * @return ティック
+     */
+    public static double secondToTick(double second) {
+        return second * 20;
+    }
+
+    /**
+     * 秒数からティックに変換します。
+     * @param second 秒数
+     * @return ティック
+     */
+    public static double secondToTick(int second) {
+        return second * 20;
+    }
+
+    /**
+     * 秒数からティックに変換します。
+     * @param second 秒数
+     * @return ティック
+     */
+    public static double secondToTick(long second) {
+        return second * 20;
+    }
+
+    /**
+     * 秒数からティックに変換します。
+     * @param second 秒数
+     * @return ティック
+     */
+    public static double secondToTick(short second) {
+        return second * 20;
     }
 
 }
